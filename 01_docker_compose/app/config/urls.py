c681e786dff976f3e9b53de9aa8cfadc11946815
lppavli <lp.pavli@gmail.com>
2022-04-22 19:16:30 +0300

    docker_compose is ready

--- 01_docker_compose/app/config/urls.py
+++ 01_docker_compose/app/config/urls.py
@@ -1,6 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
+
 from django.urls import path
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
