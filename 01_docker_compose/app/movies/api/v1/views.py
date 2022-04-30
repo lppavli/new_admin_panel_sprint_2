349ac52073d4b12c7c7238cf698e18752127535a
lppavli <lp.pavli@gmail.com>
2022-04-30 17:44:56 +0300

    errors have been fixed

--- 01_docker_compose/app/movies/api/v1/views.py
+++ 01_docker_compose/app/movies/api/v1/views.py
@@ -3,16 +3,17 @@ from django.db.models import Q
 from rest_framework import viewsets
 
 from .serializers import MovieSerializer
-from movies.models import Filmwork
+from movies.models import Filmwork, Role
+
+
+def aggredate_person(role):
+    return ArrayAgg('persons__full_name', distinct=True, filter=Q(personfilmwork__role=role))
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Filmwork.objects.prefetch_related().all().values().annotate(
-        genres=ArrayAgg("genres__name", distinct=True)).annotate(
-        directors=ArrayAgg("persons__full_name", distinct=True,
-                           filter=Q(personfilmwork__role="director"))).annotate(
-        writers=ArrayAgg("persons__full_name", distinct=True,
-                         filter=Q(personfilmwork__role="writer"))).annotate(
-        actors=ArrayAgg("persons__full_name", distinct=True,
-                        filter=Q(personfilmwork__role="actor")))
+        genres=ArrayAgg("genres__name", distinct=True),
+        directors=aggredate_person(role=Role.DIRECTOR),
+        writers=aggredate_person(role=Role.WRITER),
+        actors=aggredate_person(role=Role.ACTOR))
     serializer_class = MovieSerializer
--- 01_docker_compose/app/movies/models.py
+++ 01_docker_compose/app/movies/models.py
@@ -88,11 +88,13 @@ class Person(UUIDMixin, TimeStampedMixin):
         verbose_name_plural = _('Persons')
 
 
+class Role(models.TextChoices):
+    DIRECTOR = 'director', _('Director')
+    WRITER = 'writer', _('Writer')
+    ACTOR = 'actor', _('actor')
+
+
 class PersonFilmWork(models.Model):
-    class Role(models.TextChoices):
-        DIRECTOR = 'director', _('Director')
-        WRITER = 'writer', _('Writer')
-        ACTOR = 'actor', _('actor')
     film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
     person = models.ForeignKey('Person', on_delete=models.CASCADE)
     role = models.CharField(max_length=10,
